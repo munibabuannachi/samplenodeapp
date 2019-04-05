64ea001c4996673bc05614df2107cb482a5e3a68
munibabu.annachi <munibabu.annachi@accenture.com>
2019-04-04 21:28:46 -0400

    master

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello ... This is sample application for approval hub ");
+
+});
+
+var port = 8080;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+
